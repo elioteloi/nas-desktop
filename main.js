e4d0ddbb1c9aa914d7a413ab181825ecb6bb7bd4
Eliot Eloi <eliot.eloi22@gmail.com>
2025-05-26 11:45:46 -0300

    feat: add the preload file path

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
+require('dotenv').config();
+
 const { app, BrowserWindow } = require('electron')
+const path = require('node:path')
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
-    height: 600
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
   })
 
   win.loadFile('index.html')
