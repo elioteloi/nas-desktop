c5abcfd4c9a924606f584ed0c540657b1ffe5fb1
Eliot Eloi <eliot.eloi22@gmail.com>
2025-06-23 18:16:47 -0300

    feat: add index js file logic

--- renderer/index.js
+++ renderer/index.js
@@ -6,20 +6,15 @@ const errorSignIn = document.getElementById("errorSignIn")
 const buttonGoToLogin = document.getElementById("go-to-login")
 
 button.addEventListener('click', () => {
-  console.log(userName.value);
-  console.log(userEmail.value);
-  console.log(userPassword.value);
 
   signin(userName.value, userEmail.value, userPassword.value)
-
-  console.log("click");
 
 })
 
 
 
 async function signin(name, email, password) {
-    const response = await fetch(`http://${window.env.API_KEY}:3000/createUser`, {
+    const response = await fetch(`http://server:3000/createUser`, {
         method: 'post',
           headers: {
         'Content-Type': 'application/json',
@@ -31,14 +26,11 @@ async function signin(name, email, password) {
       })
     })
     const json = await response.json()
-    console.log("response", json);
 
     if (json.success) {
-      console.log("true");
       window.location.href = "home.html"
     } else if (json.errorEmail) {
       errorSignIn.innerHTML = json.errorEmail
-      console.log(json.errorEmail);
       
     } else if(json.errorInput) {
       errorSignIn.innerHTML = json.errorInput
@@ -47,7 +39,6 @@ async function signin(name, email, password) {
 }
 
 buttonGoToLogin.addEventListener("click", () => {
-  console.log("click login");
   window.location.href = "login.html"
 
 })
